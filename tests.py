76569fae5fdcf5ec7a5716bf39de1ab93023867c
1920xl080 <arrigo1888@gmail.com>
2025-10-25 14:59:00 +0700

    AI agent

---
+++ tests.py
@@ -0,0 +1,19 @@
+from functions.get_file_infos import get_files_info
+from functions.get_file_contents import  get_file_content
+from functions.write_file import * 
+from functions.run_python import *
+
+
+print(run_python_file("calculator", "main.py")) #(should print the calculator's usage instructions)
+
+print(run_python_file("calculator", "main.py", ["3 + 5"])) #(should run the calculator... which gives a kinda nasty rendered result)
+
+print(run_python_file("calculator", "tests.py"))
+
+print(run_python_file("calculator", "../main.py")) #(this should return an error)
+
+print(run_python_file("calculator", "nonexistent.py")) #(this should return an error)
+
+
+
+
